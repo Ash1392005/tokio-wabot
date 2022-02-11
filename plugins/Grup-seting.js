0fd2035a0cd0918eccdf1077f28a0bec443bcf03
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-02-11 19:21:16 +0530

    Create Grup-seting.js

---
+++ plugins/Grup-seting.js
@@ -0,0 +1,45 @@
+let { GroupSettingChange } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let isClose = {
+    'open': false,
+    'buka': false,
+    'on': false,
+    '1': false,
+    'close': true,
+    'tutup': true,
+    'off': true,
+    '0': true,
+  }[(args[0] || '')]
+  if (isClose === undefined) {
+    await conn.send2Button(m.chat, `
+⚠️ Use :
+
+${usedPrefix + command} <open/close>
+
+⚠️ Example :
+
+${usedPrefix + command} close
+${usedPrefix + command} open
+
+🔺 If The Bottons Not Working Use The CMDs..
+ `.trim(), '© falling 🍂', '◽️ᴏᴘᴇɴ ◽️', '.group open', '◾️ ᴄʟᴏsᴇ ◾️', '.group close', m)
+    throw 0
+  }
+  await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
+}
+handler.help = ['grup <open/close>']
+handler.tags = ['group']
+handler.command = /^(gro?up)$/i
+
+handler.admin = true
+handler.botAdmin = true
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+handler.exp = 0
+module.exports = handler
