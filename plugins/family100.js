66f9ded070c3cf727f6b7b95aaaba3b95bd7cc42
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-02-13 18:03:31 +0530

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -1,35 +1,46 @@
-let fetch = require('node-fetch')
-let winScore = 500
-async function handler(m) {
-    this.game = this.game ? this.game : {}
-    let id = 'family100_' + m.chat
-    if (id in this.game) {
-        this.reply(m.chat, 'There are still unanswered quizzes in this chat', this.game[id].msg)
-        throw false
-    }
-    let res = await fetch(global.API('xteam', '/game/family100', {}, 'APIKEY'))
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
-    let caption = `
-*About:* ${json.soal}
+const similarity = require('similarity')
+const threshold = 0.72 // semakin tinggi nilai, semakin mirip
+module.exports = {
+    async before(m) {
+        this.game = this.game ? this.game : {}
+        let id = 'family100_' + m.chat
+        if (!(id in this.game)) return !0
+        let room = this.game[id]
+        let text = m.text.toLowerCase().replace(/[^\w\s\-]+/, '')
+        let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
+        if (!isSurrender) {
+            let index = room.jawaban.indexOf(text)
+            if (index < 0) {
+                if (Math.max(...room.jawaban.filter((_, index) => !room.terjawab[index]).map(jawaban => similarity(jawaban, text))) >= threshold) m.reply('Dikit lagi!')
+                return !0
+            }
+            if (room.terjawab[index]) return !0
+            let users = global.db.data.users[m.sender]
+            room.terjawab[index] = m.sender
+            users.exp += room.winScore
+        }
+        let isWin = room.terjawab.length === room.terjawab.filter(v => v).length
+        let caption = `
+*Soal:* ${room.soal}
 
-There is *${json.answer}* answer${json.answer.find(v => v.includes(' ')) ? `
-(some answers have spaces)
+Terdapat *${room.jawaban.length}* jawaban${room.jawaban.find(v => v.includes(' ')) ? `
+(beberapa jawaban terdapat spasi)
 `: ''}
+${isWin ? `*SEMUA JAWABAN TERJAWAB*` : isSurrender ? '*MENYERAH!*' : ''}
+${Array.from(room.jawaban, (jawaban, index) => {
+            return isSurrender || room.terjawab[index] ? `(${index + 1}) ${jawaban} ${room.terjawab[index] ? '@' + room.terjawab[index].split('@')[0] : ''}`.trim() : false
+        }).filter(v => v).join('\n')}
 
-+${winScore} XP every answer is correct
+${isSurrender ? '' : `+${room.winScore} XP tiap jawaban benar`}
     `.trim()
-    this.game[id] = {
-        id,
-        msg: await this.sendButton(m.chat, caption, author, 'give up', 'giveup', m),
-        ...json,
-        answerd: Array.from(json.answer, () => false),
-        winScore,
+        await this.sendButton(m.chat, caption, author, `${(isWin || isSurrender) ? 'Family 100' : 'Nyerah'}`, `${(isWin || isSurrender) ? '.family100' : 'nyerah'}`, null, {
+            contextInfo: {
+                mentionedJid: this.parseMention(caption)
+            }
+        }).then(msg => {
+            return this.game[id].msg = msg
+        }).catch(_ => _)
+        if (isWin || isSurrender) delete this.game[id]
+        return !0
     }
 }
-handler.help = ['family100']
-handler.tags = ['game']
-handler.command = /^family100$/i
-
-module.exports = handler
